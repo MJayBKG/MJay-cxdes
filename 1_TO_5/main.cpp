140be45b097244586e7a2278bfb4740e03732a78
Jarrel <jarrelmjay@icloud.com>
2025-05-02 23:51:17 +0200

    Initial Commit

---
+++ 1_TO_5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1_TO_5
+//
+//  Created by JarrelMJay on 2025/05/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
